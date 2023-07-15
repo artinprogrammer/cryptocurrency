9b6f268f0eee4433be7ae13a58a38e598147b245
artinprogrammer <goldenboy20081387@gmail.com>
2023-07-15 19:46:52 +0330

    first commit

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants{
+  // all pages
+  static Color scfBackgroundColor = const Color.fromRGBO(19,0,65,1.000);
+  static Color cryptoItemBackgroundColor = const Color.fromRGBO(40,27,84,1.000);
+  // home
+  static Color trendingBottomRightGradient = const Color.fromRGBO(144,74,182,1.000);
+  static Color trendingTopLeftGradient = const Color.fromRGBO(91,74,218,1.000);
+  // intro
+  static Color introContainerTopRightGradient = const Color.fromRGBO(200,246,154,1.000);
+  static Color introContainerBottomLeftGradient = const Color.fromRGBO(152,96,229,1.000);
+  static Color introBottonTopRightGradient = const Color.fromRGBO(107,86,224,1.000);
+  static Color introBottonBottomLeftGradient = const Color.fromRGBO(206,89,250,1.000);
+  //bottomNavigation
+  static Color bottomNavigationGlassColor = const Color.fromRGBO(42,26,98,1.000);
+  static Color bottomNavigationSelectedTabColor = const Color.fromRGBO(206,89,250,1.000);
+
+}
